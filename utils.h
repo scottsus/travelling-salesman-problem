fc31f5f13e599e15454c53b78a1cbef3fbcca135
Scott Susanto <scottsusanto@yahoo.com>
2022-12-04 23:45:09 -0800

    move data to data folder

--- utils.h
+++ utils.h
@@ -14,46 +14,46 @@ int V;
 std::vector<int> init_distance_matrix(int n)
 {
     V = n;
-	char filename[20];
-	sprintf(filename, "data%d.txt", n);
-	cout << "Opening " << filename << "..." << endl;
+    char filename[20];
+    sprintf(filename, "data/data%d.txt", n);
+    cout << "Opening " << filename << "..." << endl;
 
-	ifstream file;
+    ifstream file;
     file.open(filename);
-    if (!file.is_open()){
+    if (!file.is_open())
+    {
         cout << "Incorrect filename" << endl;
-		vector<int> null;
+        vector<int> null;
         return null;
     }
 
     int graph[V][V];
-    for (int i = 0; i != V; i++) {
+    for (int i = 0; i != V; i++)
+    {
         for (int j = 0; j != V; j++)
-          file >> graph[i][j];
+            file >> graph[i][j];
     }
 
- 	std::vector<int> distances(V * V);
-    for (int from = 0; from < V; from++) {
+    std::vector<int> distances(V * V);
+    for (int from = 0; from < V; from++)
+    {
         for (int to = 0; to < V; to++)
             distances[to + V * from] = graph[to][from];
     }
 
-  	return distances;
-} 
-
+    return distances;
+}
 
 std::vector<int> init_distance_matrix_small(int n)
 {
-  auto distances = init_distance_matrix(n);
-  std::vector<int> distances_small(V * V);
-  for (int from = 0; from < n; from++)
-  {
-  	for (int to = 0; to < n; to++)
-    	distances_small[to + V * from] = distances[to + V * from];
-  }
-  return distances_small;
-
-} 
+    auto distances = init_distance_matrix(n);
+    std::vector<int> distances_small(V * V);
+    for (int from = 0; from < n; from++)
+    {
+        for (int to = 0; to < n; to++)
+            distances_small[to + V * from] = distances[to + V * from];
+    }
+    return distances_small;
+}
 
 #endif
-
