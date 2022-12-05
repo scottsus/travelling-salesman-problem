9ce33f61d4617784d4449f15b6f215a86ef8274e
Scott Susanto <scottsusanto@yahoo.com>
2022-12-05 00:14:23 -0800

    format files

--- route_iterator.h
+++ route_iterator.h
@@ -8,90 +8,92 @@
 
 uint64_t factorial(uint64_t n)
 {
-    if (n <= 1)
-    	return 1;
-    return n * factorial(n - 1);
+	if (n <= 1)
+		return 1;
+	return n * factorial(n - 1);
 }
 
-void integer_divide(uint64_t original, uint64_t divisor, uint64_t& quotient, uint64_t& remainder)
+void integer_divide(uint64_t original, uint64_t divisor, uint64_t &quotient, uint64_t &remainder)
 {
 	quotient = original / divisor;
-  	remainder = original - divisor * quotient;
+	remainder = original - divisor * quotient;
 }
 
 template <typename T>
-void swap(T& a, T& b)
+void swap(T &a, T &b)
 {
-    T c(a); a = b; b = c;
+	T c(a);
+	a = b;
+	b = c;
 }
 
 template <int N>
 class route_iterator
 {
 public:
-  	route_iterator(uint64_t route_id);
+	route_iterator(uint64_t route_id);
 
-  	bool done() const {
-  		return m_i == (N - 1);
+	bool done() const
+	{
+		return m_i == (N - 1);
 	};
 
-  	int first() {
+	int first()
+	{
 		m_i = 0;
 		return state[0];
 	};
 
-  	int next() {
+	int next()
+	{
 		m_i++;
 		return state[m_i];
 	};
 
-  	void print();
+	void print();
 
 private:
-  	uint64_t route_id;
-  	int m_i;
-  	int state[N];
-
+	uint64_t route_id;
+	int m_i;
+	int state[N];
 };
 
-template<int N>
+template <int N>
 route_iterator<N>::route_iterator(uint64_t route_id)
-  : route_id(route_id),
-    m_i(0)
+	: route_id(route_id),
+	  m_i(0)
 {
-  assert(route_id < factorial(N));
+	assert(route_id < factorial(N));
 
-  	for (int k = 0; k < N; ++k)
-    	state[k] = -1;
+	for (int k = 0; k < N; ++k)
+		state[k] = -1;
 
-  	for (int k = 0; k < N; ++k)
-    	state[k] = k;
+	for (int k = 0; k < N; ++k)
+		state[k] = k;
 
+	assert(N >= 2);
 
-  	assert(N >= 2);
+	int divisor = 1;
+	uint64_t iter = route_id;
+	for (int k = 1; k < N; ++k)
+	{
 
-  	int divisor = 1;
- 	uint64_t iter = route_id;
-  	for(int k = 1; k < N; ++k)
-  	{
+		uint64_t quotient, remainder;
+		integer_divide(iter / divisor, k + 1, quotient, remainder);
 
-    	uint64_t quotient, remainder;
-    	integer_divide(iter/divisor, k + 1, quotient, remainder);
-
-    	if (remainder > 0)
-      		swap(state[k], state[k - remainder]);
-    	divisor *= (k + 1);
-  	}
+		if (remainder > 0)
+			swap(state[k], state[k - remainder]);
+		divisor *= (k + 1);
+	}
 }
 
-template<int N>
+template <int N>
 void route_iterator<N>::print()
 {
 	printf("Optimal Route:\n");
-	for (int k=0; k<N; ++k)
-    	printf("%2d ", state[k]);
-  	printf("\n");
+	for (int k = 0; k < N; ++k)
+		printf("%2d ", state[k]);
+	printf("\n");
 }
 
 #endif
-
--- timer.h
+++ timer.h
@@ -24,13 +24,13 @@ public:
 
     double elapsedMilliseconds()
     {
-      	std::chrono::time_point<std::chrono::system_clock> endTime;
+        std::chrono::time_point<std::chrono::system_clock> endTime;
 
-      	if(m_bRunning)
-        	endTime = std::chrono::system_clock::now();
-     	else
-        	endTime = m_EndTime;
-      	return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - m_StartTime).count();
+        if (m_bRunning)
+            endTime = std::chrono::system_clock::now();
+        else
+            endTime = m_EndTime;
+        return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - m_StartTime).count();
     }
 
     double elapsedSeconds()
@@ -39,8 +39,7 @@ public:
     }
 
 private:
-  	std::chrono::time_point<std::chrono::system_clock> m_StartTime;
-  	std::chrono::time_point<std::chrono::system_clock> m_EndTime;
-  	bool m_bRunning = false;
+    std::chrono::time_point<std::chrono::system_clock> m_StartTime;
+    std::chrono::time_point<std::chrono::system_clock> m_EndTime;
+    bool m_bRunning = false;
 };
-
